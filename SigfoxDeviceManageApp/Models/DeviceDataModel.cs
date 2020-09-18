4e127994dc92c93e1bb5b15327a0afa9fed61633
OTakumi <takumi.o.0410@gmail.com>
2020-09-18 18:05:47 +0900

    Add Device data model

---
+++ SigfoxDeviceManageApp/Models/DeviceDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SigfoxDeviceManageApp.Models
+{
+    public class DeviceDataModel
+    {
+        private Guid deviceId { get; set; }
+        public string deviceName { get; set; }
+        public DateTime resisteDateTime { get; set; }
+        public double temperature { get; set; }
+        public int humidity { get; set; }
+    }
+}
