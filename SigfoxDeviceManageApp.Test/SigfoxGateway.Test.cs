a316474f232d0afb8803bb13a0e9760bb4c68d4c
oTakumi <takumi.o.0410@gmail.com>
2020-09-29 16:25:55 +0900

    fix get device message function and add unit test

---
+++ SigfoxDeviceManageApp.Test/SigfoxGateway.Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SigfoxDeviceManageApp.Tests
+{
+    class SigfoxGateway
+    {
+        [Fact]
+        public void MessageAPIResponseCheck(string deviceId)
+        {
+            // 200
+        }
+    }
+}
--- SigfoxDeviceManageApp/Controllers/SigfoxAPIGateway.cs
+++ SigfoxDeviceManageApp/Controllers/SigfoxAPIGateway.cs
@@ -1,15 +1,54 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SigfoxDeviceManageApp.Controllers
 {
     public class SigfoxAPIGateway
     {
-        public static void GetMessage()
+        /// <summary>
+        /// Getting Data from the Sigfox Cloud API.
+        /// </summary>
+        /// <returns>return jObject data</returns>
+        public static async Task<JObject> GetMessage(string deviceId, string loginId, string password)
         {
+            var messageUri = "https://api.sigfox.com/v2/devices/" + deviceId + "/messages";
 
+            // Http request get
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(messageUri)
+            };
+
+            // Add Header for Basic Authorization
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
+                                            Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", loginId, password))));
+
+            JObject messageData;
+
+            // Get data
+            using (var httpClient = new HttpClient())
+            {
+                // Send a request to the API URI and check the response.
+                var response = await httpClient.SendAsync(request);
+
+                // If the authentication is successful, return the value obtained.
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Basic Authorization is Succeeded");
+                    return messageData = JObject.Parse(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    Console.WriteLine("Basic Authorization Error");
+                    return null;
+                }
+            }
         }
 
         private static bool RegisterDevice()
