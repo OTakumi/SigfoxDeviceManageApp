8f05134cf77040860c40675d0fd4775bdf08e3ab
oTakumi <takumi.o.0410@gmail.com>
2020-10-07 17:16:01 +0900

    fix sigfox index page

--- SigfoxDeviceManageApp/Pages/Sigfox/GetMessage.cshtml.cs
+++ SigfoxDeviceManageApp/Pages/Sigfox/GetMessage.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,41 +14,41 @@ namespace SigfoxDeviceManageApp.Pages.Sigfox
         public void OnGet()
         {
         }
-    }
 
-    public static async Task<object> GetMessage(string deviceId = "", string loginId = "", string password = "")
-    {
-        var messageUri = "https://api.sigfox.com/v2/devices/" + deviceId + "/messages";
-
-        // Http request get
-        var request = new HttpRequestMessage
+        public static async Task<object> GetMessage(string deviceId = "", string loginId = "", string password = "")
         {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri(messageUri)
-        };
+            var messageUri = "https://api.sigfox.com/v2/devices/" + deviceId + "/messages";
 
-        // Add Header for Basic Authorization
-        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
-                                        Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", loginId, password))));
-
-        string messageData;
-
-        // Get data
-        using (var httpClient = new HttpClient())
-        {
-            // Send a request to the API URI and check the response.
-            var response = await httpClient.SendAsync(request);
-
-            // If the authentication is successful, return the value obtained.
-            if (response.IsSuccessStatusCode)
+            // Http request get
+            var request = new HttpRequestMessage
             {
-                Console.WriteLine("Basic Authorization is Succeeded");
-                return messageData = "Get success";
-            }
-            else
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(messageUri)
+            };
+
+            // Add Header for Basic Authorization
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
+                                            Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", loginId, password))));
+
+            string messageData;
+
+            // Get data
+            using (var httpClient = new HttpClient())
             {
-                Console.WriteLine("Basic Authorization Error");
-                return null;
+                // Send a request to the API URI and check the response.
+                var response = await httpClient.SendAsync(request);
+
+                // If the authentication is successful, return the value obtained.
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Basic Authorization is Succeeded");
+                    return messageData = "Get success";
+                }
+                else
+                {
+                    Console.WriteLine("Basic Authorization Error");
+                    return null;
+                }
             }
         }
     }
